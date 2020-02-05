e8bb944121db9eb94d4513053d1f91cad9981ca9
Sibtain Jafferi <msajaffe@uwaterloo.ca>
2020-02-04 19:09:48 -0800

    using svelte-:comp to conditionally render modal
    Signed-off-by: Sibtain Jafferi <msajaffe@uwaterloo.ca>

--- src/Images/Modal.svelte
+++ src/Images/Modal.svelte
@@ -1,14 +1,10 @@
 <script>
   // from https://github.com/flekschas/svelte-simple-modal
+  import ModalBg from "./ModalBg.svelte";
   import { setContext as baseSetContext } from "svelte";
-  import { fade } from "svelte/transition";
   export let key = "simple-modal";
   export let closeOnEsc = true;
   export let closeOnOuterClick = true;
-  export let transitionBg = fade;
-  export let transitionBgProps = { duration: 250 };
-  export let transitionWindow = transitionBg;
-  export let transitionWindowProps = transitionBgProps;
   export let styleBg = { top: 0, left: 0 };
   export let styleWindow = {};
   export let styleContent = {};
@@ -74,49 +70,18 @@
   * {
     box-sizing: border-box;
   }
-  .bg {
-    position: fixed;
-    z-index: 1000;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.66);
-    transition: opacity 200ms ease 0s;
-  }
-  .window-wrap {
-    position: relative;
-  }
-  .window {
-    position: relative;
-  }
-  .content {
-    position: relative;
-  }
 </style>
 
 <svelte:window on:keyup={handleKeyup} />
 
 <div>
-  {#if isOpen}
-    <div
-      class="bg"
-      on:click={handleOuterClick}
-      bind:this={background}
-      transition:transitionBg={transitionBgProps}
-      style={cssBg}>
-      <div class="window-wrap" bind:this={wrap}>
-        <div
-          class="window"
-          transition:transitionWindow={transitionWindowProps}
-          style={cssWindow}>
-          <div class="content" style={cssContent}>
-            <svelte:component this={Component} {...props} />
-          </div>
-        </div>
-      </div>
-    </div>
-  {/if}
+  <svelte:component
+    this={isOpen ? ModalBg : null}
+    {handleOuterClick}
+    {background}
+    {wrap}
+    {Component}>
+    <svelte:component this={Component} {...props} />
+  </svelte:component>
   <slot />
 </div>
---
+++ src/Images/ModalBg.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { fade } from "svelte/transition";
+  export let transitionBg = fade;
+  export let transitionBgProps = { duration: 250 };
+  export let transitionWindow = transitionBg;
+  export let transitionWindowProps = transitionBgProps;
+  export let handleOuterClick;
+  export let background;
+  export let wrap;
+  export let Component;
+</script>
+
+<style>
+  .bg {
+    position: fixed;
+    z-index: 1000;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.66);
+    transition: opacity 200ms ease 0s;
+  }
+  .window-wrap {
+    position: relative;
+  }
+  .window {
+    position: relative;
+  }
+  .content {
+    position: relative;
+  }
+</style>
+
+<div
+  class="bg"
+  on:click={handleOuterClick}
+  bind:this={background}
+  transition:transitionBg={transitionBgProps}>
+  <div class="window-wrap" bind:this={wrap}>
+    <div class="window" transition:transitionWindow={transitionWindowProps}>
+      <div class="content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</div>
